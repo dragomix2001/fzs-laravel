801b12c310da7ce119956dc1008b6c8a6a2df431
Dragan <dragomix2001@gmail.com>
2026-03-14 23:11:23 +0100

    Configure Vite bundling with Tailwind CSS and jQuery for frontend assets

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,43 @@
 import './bootstrap';
+import $ from 'jquery';
+import * as bootstrap from 'bootstrap';
+
+window.$ = window.jQuery = $;
+window.bootstrap = bootstrap;
+
+$(document).ready(function() {
+    initializeTooltips();
+    initializeDropdowns();
+    initializeSidebar();
+});
+
+function initializeTooltips() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+}
+
+function initializeDropdowns() {
+    document.querySelectorAll('[data-bs-toggle="dropdown"]').forEach(function(dropdownToggle) {
+        new bootstrap.Dropdown(dropdownToggle);
+    });
+}
+
+function initializeSidebar() {
+    document.querySelectorAll('#side-menu li a').forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            var parent = this.parentElement;
+            var submenu = parent.querySelector('.nav-second-level, .nav-third-level');
+            
+            if (submenu) {
+                e.preventDefault();
+                submenu.classList.toggle('d-none');
+            }
+        });
+    });
+}
+
+if (typeof window.DataTable !== 'undefined') {
+    $.fn.DataTable = window.DataTable;
+}
