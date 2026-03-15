7d64aaeb8deef03e1a9c59b56b12b4286582905a
Dragan <dragomix2001@gmail.com>
2026-03-15 23:49:35 +0100

    Add jQuery 4.0 to all layouts, fix /zapisnik/create AJAX functionality

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -457,6 +457,7 @@
         </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/4.0.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
     
