d7a617540a1bc1eb6995d8c46f0b870c11feb838
Dragan <dragomix2001@gmail.com>
2026-03-16 06:41:49 +0100

    Fix CI - add npm run build and default role in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
-            $table->string('role');
+            $table->string('role')->default('user');
             $table->timestamps();
         });
     }
