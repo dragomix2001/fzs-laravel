c434999d07b73eaf58d5e3d5c67d7281cc3f5024
Dragan <dragomix2001@gmail.com>
2026-04-05 15:49:52 +0200

    Fix Pint formatting for ApiDocsControllerTest

--- tests/Feature/ApiDocsControllerTest.php
+++ tests/Feature/ApiDocsControllerTest.php
@@ -12,32 +12,32 @@ class ApiDocsControllerTest extends TestCase
 {
     public function test_index_returns_view_instance(): void
     {
-        $controller = new ApiDocsController();
+        $controller = new ApiDocsController;
         $response = $controller->index();
-        
+
         $this->assertInstanceOf(View::class, $response);
     }
 
     public function test_index_returns_api_docs_view(): void
     {
-        $controller = new ApiDocsController();
+        $controller = new ApiDocsController;
         $response = $controller->index();
-        
+
         $this->assertEquals('api.docs', $response->getName());
     }
 
     public function test_index_has_correct_view_path(): void
     {
-        $controller = new ApiDocsController();
+        $controller = new ApiDocsController;
         $response = $controller->index();
-        
+
         $this->assertStringContainsString('api', $response->getName());
         $this->assertStringContainsString('docs', $response->getName());
     }
 
     public function test_controller_index_method_exists(): void
     {
-        $controller = new ApiDocsController();
+        $controller = new ApiDocsController;
         $this->assertTrue(method_exists($controller, 'index'));
     }
 }
