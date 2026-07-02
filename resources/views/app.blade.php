b3ea67eb331127da42de5773099b5794e5e6df3b
Dragan <dragomix2001@gmail.com>
2026-07-02 08:14:13 +0200

    refactor: migrate login and app layout from Bootstrap to Tailwind
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,60 +7,52 @@
 	<title>Laravel</title>
 
 	<link href="/css/app.css" rel="stylesheet">
-
-	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
+	<nav class="bg-white shadow-sm border-b border-secondary-200">
+		<div class="max-w-7xl mx-auto px-4">
+			<div class="flex items-center justify-between h-14">
+				<div class="flex items-center gap-4">
+					<button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-secondary-500 hover:bg-secondary-100 lg:hidden" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+						<span class="sr-only">Toggle Navigation</span>
+						<i class="fas fa-bars"></i>
+					</button>
+					<a class="text-lg font-bold text-secondary-900" href="#">Laravel</a>
+				</div>
 
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="/">Home</a></li>
-				</ul>
+				<div class="hidden lg:flex lg:items-center lg:gap-4" id="navbarNav">
+					<ul class="flex items-center gap-1">
+						<li><a href="/" class="px-3 py-2 text-sm font-medium text-secondary-700 hover:text-secondary-900 hover:bg-secondary-50 rounded-md transition-colors">Home</a></li>
+					</ul>
 
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
-						<li><a href="/auth/register">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li>
-									<form method="POST" action="{{ route('logout') }}" style="margin: 0;">
-										{{ csrf_field() }}
-										<a href="#" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
-									</form>
-								</li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+					<ul class="flex items-center gap-1">
+						@if (Auth::guest())
+							<li><a href="/auth/login" class="px-3 py-2 text-sm font-medium text-secondary-700 hover:text-secondary-900 hover:bg-secondary-50 rounded-md transition-colors">Login</a></li>
+							<li><a href="/auth/register" class="px-3 py-2 text-sm font-medium text-secondary-700 hover:text-secondary-900 hover:bg-secondary-50 rounded-md transition-colors">Register</a></li>
+						@else
+							<li x-data="{ open: false }" class="relative">
+								<a href="#" @click.prevent="open = !open" class="px-3 py-2 text-sm font-medium text-secondary-700 hover:text-secondary-900 hover:bg-secondary-50 rounded-md inline-flex items-center transition-colors" role="button">
+									{{ Auth::user()->name }} <i class="fas fa-chevron-down ml-1 text-xs"></i>
+								</a>
+								<ul x-show="open" @click.away="open = false" class="absolute right-0 mt-1 w-48 bg-white rounded-lg shadow-lg border border-secondary-200 py-1 z-50" x-transition>
+									<li>
+										<form method="POST" action="{{ route('logout') }}" style="margin: 0;">
+											{{ csrf_field() }}
+											<a href="#" class="block px-4 py-2 text-sm text-secondary-700 hover:bg-secondary-50" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+										</form>
+									</li>
+								</ul>
+							</li>
+						@endif
+					</ul>
+				</div>
 			</div>
 		</div>
 	</nav>
 
 	@yield('content')
 
-	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,32 +1,31 @@
 @extends ('layouts.plane')
 @section ('body')
-<div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-            <br /><br /><br />
-               @section ('login_panel_title','Please Sign In')
-               @section ('login_panel_body')
-                        <form role="form">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <a href="{{ url ('') }}" class="btn btn-lg btn-success btn-block">Login</a>
-                            </fieldset>
-                        </form>
-                    
-                @endsection
-                @include('widgets.panel', array('as'=>'login', 'header'=>true))
+<div class="min-h-screen flex items-center justify-center bg-secondary-100 px-4">
+    <div class="w-full max-w-sm">
+        <div class="bg-white rounded-lg border border-secondary-200 shadow-sm overflow-hidden">
+            <div class="px-6 py-4 border-b border-secondary-200 bg-secondary-50">
+                <h4 class="text-lg font-semibold text-secondary-900 text-center">Please Sign In</h4>
+            </div>
+            <div class="p-6">
+                <form role="form">
+                    <fieldset class="space-y-4">
+                        <div class="space-y-1">
+                            <label class="block text-sm font-medium text-secondary-700">E-mail</label>
+                            <input class="block w-full rounded-lg border-secondary-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm" placeholder="E-mail" name="email" type="email" autofocus>
+                        </div>
+                        <div class="space-y-1">
+                            <label class="block text-sm font-medium text-secondary-700">Password</label>
+                            <input class="block w-full rounded-lg border-secondary-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm" placeholder="Password" name="password" type="password">
+                        </div>
+                        <label class="flex items-center gap-2">
+                            <input name="remember" type="checkbox" class="rounded border-secondary-300 text-primary-600 focus:ring-primary-500">
+                            <span class="text-sm text-secondary-700">Remember Me</span>
+                        </label>
+                        <a href="{{ url('') }}" class="inline-flex items-center justify-center w-full px-4 py-2 bg-success-600 hover:bg-success-500 text-white text-sm font-medium rounded-lg transition-colors">Login</a>
+                    </fieldset>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @stop
