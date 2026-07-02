50e9accaea6e58ce3d60efebb1368e98d3f826be
Dragan <dragomix2001@gmail.com>
2026-07-02 08:14:18 +0200

    refactor: migrate table and documentation views from Bootstrap to Tailwind
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -4,87 +4,67 @@
 
 @section('section')
 
-		@section ('dpanel_panel_title', 'Panel')
-		@section ('dpanel_panel_body')
-			@section ('inside_panel_title', 'Default title')
-			@section ('inside_panel_body')
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			@endsection
-			@include('widgets.panel', array('header'=>true, 'as'=>'inside'))
-		@endsection	
-		@section ('dpanel_panel_footer')
-<pre class="codeWrapper">
-<i class="fa fa-at"></i>section ('inside_panel_title', 'Default title')
-<i class="fa fa-at"></i>section ('inside_panel_body')
-&lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo&lt;/p&gt;
-<i class="fa fa-at"></i>endsection
-<i class="fa fa-at"></i>include('widgets.panel', array('header'=>true, 'as'=>'inside'))
-</pre>
-		@endsection
-		@include ('widgets.panel', array('class'=>'primary', 'header'=>true, 'footer'=>true, 'as'=>'dpanel'))
+<x-card>
+    <x-slot:title>Panel</x-slot:title>
+    <x-card>
+        <x-slot:title>Default title</x-slot:title>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    </x-card>
+    <div class="mt-3 bg-secondary-50 rounded-lg p-4 text-xs font-mono text-secondary-600 overflow-x-auto">
+        <div>@section ('inside_panel_title', 'Default title')</div>
+        <div>@section ('inside_panel_body')</div>
+        <div>&lt;p&gt;Lorem ipsum...&lt;/p&gt;</div>
+        <div>@endsection</div>
+        <div>@include('widgets.panel', array('header'=>true, 'as'=>'inside'))</div>
+    </div>
+</x-card>
 
+<x-card>
+    <x-slot:title>Collapsible</x-slot:title>
+    @include('widgets.collapse', array('id'=>'2', 'header'=> 'This is a header', 'body'=>'Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.'))
+    <div class="mt-3 bg-secondary-50 rounded-lg p-4 text-xs font-mono text-secondary-600 overflow-x-auto">
+        @include('widgets.collapse', array('id'=>'2', 'header'=> 'This is a header', 'body'=>'Nihil anim...'))
+    </div>
+</x-card>
 
-		
-		@section ('dcollapsible_panel_title', 'Collapssible')
-		@section ('dcollapsible_panel_body')
-			@include('widgets.collapse', array('id'=>'2', 'header'=> 'This is a header', 'body'=>'Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.'))
-		@endsection	
-		@section ('dcollapsible_panel_footer')
-<pre class="codeWrapper">
-<i class="fa fa-at"></i>include('widgets.collapse', array('id'=>'2', 'header'=> 'This is a header', 'body'=>'Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.'))
-</pre>
-		@endsection
-		@include ('widgets.panel', array('class'=>'primary', 'header'=>true, 'footer'=>true, 'as'=>'dcollapsible'))
+<x-card>
+    <x-slot:title>Button</x-slot:title>
+    <div class="flex flex-wrap gap-2 mb-4">
+        @include('widgets.button', array('value'=>'Info button', 'class'=>'info'))
+        @include('widgets.button', array('class'=>'danger', 'size'=>'lg', 'value'=>'Large Button'))
+        @include('widgets.button', array('class'=>'success btn-outline', 'value'=>'Primary'))
+    </div>
+    <div class="bg-secondary-50 rounded-lg p-4 text-xs font-mono text-secondary-600 overflow-x-auto">
+        <div>@include('widgets.button', array('value'=>'Info button', 'class'=>'info'))</div>
+        <div>@include('widgets.button', array('class'=>'danger', 'size'=>'lg', 'value'=>'Large Button'))</div>
+        <div>@include('widgets.button', array('class'=>'success btn-outline', 'value'=>'Primary'))</div>
+    </div>
+</x-card>
 
-		
+<x-card>
+    <x-slot:title>Alerts</x-slot:title>
+    <div class="space-y-2 mb-4">
+        @include('widgets.alert', array('class'=>'success', 'message'=> 'You have an alert', 'icon'=> 'user'))
+        @include('widgets.alert', array('class'=>'info', 'dismissable'=>true, 'message'=> 'My message'))
+    </div>
+    <div class="bg-secondary-50 rounded-lg p-4 text-xs font-mono text-secondary-600 overflow-x-auto">
+        <div>@include('widgets.alert', array('class'=>'success', 'message'=> 'You have an alert', 'icon'=> 'user'))</div>
+        <div>@include('widgets.alert', array('class'=>'info', 'dismissable'=>true, 'message'=> 'My message'))</div>
+    </div>
+</x-card>
 
-		@section ('dbutton_panel_title', 'Button')
-		@section ('dbutton_panel_body')
-			@include('widgets.button', array('value'=>'Info button', 'class'=>'info'))
-			@include('widgets.button', array('class'=>'danger', 'size'=>'lg', 'value'=>'Large Button'))
-			@include('widgets.button', array('class'=>'success btn-outline', 'value'=>'Primary'))
-		@endsection	
-		@section ('dbutton_panel_footer')
-<pre class="codeWrapper">
-<i class="fa fa-at"></i>include('widgets.button', array('value'=>'Info button', 'class'=>'info'))
-<i class="fa fa-at"></i>include('widgets.button', array('class'=>'danger', 'size'=>'lg', 'value'=>'Large Button'))
-<i class="fa fa-at"></i>include('widgets.button', array('class'=>'success btn-outline', 'value'=>'Primary'))
-</pre>
-		@endsection
-		@include ('widgets.panel', array('class'=>'primary', 'header'=>true, 'footer'=>true, 'as'=>'dbutton'))
+<x-card>
+    <x-slot:title>Progressbars</x-slot:title>
+    <div class="space-y-2 mb-4">
+        @include('widgets.progress', array('class'=> 'success', 'value'=>'44'))
+        @include('widgets.progress', array('animated'=> true, 'value'=>'72'))
+        @include('widgets.progress', array('class'=> 'danger', 'value'=>'12', 'badge'=>true))
+    </div>
+    <div class="bg-secondary-50 rounded-lg p-4 text-xs font-mono text-secondary-600 overflow-x-auto">
+        <div>@include('widgets.progress', array('class'=> 'success', 'value'=>'44'))</div>
+        <div>@include('widgets.progress', array('animated'=> true, 'value'=>'72'))</div>
+        <div>@include('widgets.progress', array('class'=> 'danger', 'value'=>'12', 'badge'=>true))</div>
+    </div>
+</x-card>
 
-		
-
-
-		@section ('doalert_panel_title', 'Alerts')
-		@section ('doalert_panel_body')
-			@include('widgets.alert', array('class'=>'success', 'message'=> 'You have an alert', 'icon'=> 'user'))
-			@include('widgets.alert', array('class'=>'info', 'dismissable'=>true, 'message'=> 'My message'))
-		@endsection	
-		@section ('doalert_panel_footer')
-<pre class="codeWrapper">
-<i class="fa fa-at"></i>include('widgets.alert', array('class'=>'success', 'message'=> 'You have an alert', 'icon'=> 'user'))
-<i class="fa fa-at"></i>include('widgets.alert', array('class'=>'info', 'dismissable'=>true, 'message'=> 'My message'))
-</pre>
-		@endsection
-		@include ('widgets.panel', array('class'=>'primary', 'header'=>true, 'footer'=>true, 'as'=>'doalert'))
-
-		
-
-		@section ('dprogress_panel_title', 'Progressbars')
-		@section ('dprogress_panel_body')
-			@include('widgets.progress', array('class'=> 'success', 'value'=>'44'))
-			@include('widgets.progress', array('animated'=> true, 'value'=>'72'))
-			@include('widgets.progress', array('class'=> 'danger', 'value'=>'12', 'badge'=>true))
-		@endsection	
-		@section ('dprogress_panel_footer')
-<pre class="codeWrapper">
-<i class="fa fa-at"></i>include('widgets.progress', array('class'=> 'success', 'value'=>'44'))
-<i class="fa fa-at"></i>include('widgets.progress', array('animated'=> true, 'value'=>'72'))
-<i class="fa fa-at"></i>include('widgets.progress', array('class'=> 'danger', 'value'=>'12', 'badge'=>true))
-</pre>
-		@endsection
-		@include ('widgets.panel', array('class'=>'primary', 'header'=>true, 'footer'=>true, 'as'=>'dprogress'))
-
-	
 @stop
--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -2,109 +2,93 @@
 @section('page_heading','Tables')
 
 @section('section')
-<div class="col-sm-12">
-<div class="row">
-	<div class="col-sm-6">
-		@section ('table_panel_title','Regular Table')
-		@section ('table_panel_body')
-		@include('widgets.table', array('class'=>''))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'table'))
+<div class="col-span-12">
+<div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+	<div>
+		<x-card>
+			<x-slot:title>Regular Table</x-slot:title>
+			@include('widgets.table', array('class'=>''))
+		</x-card>
 	</div>
-	<div class="col-sm-6">
-		@section ('btable_panel_title','Bordered Table')
-		@section ('btable_panel_body')
-		@include('widgets.table', array('class'=>'table-bordered'))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'btable'))
-	</div>
-</div>
-<div class="row">
-	<div class="col-sm-6">
-	@section ('stable_panel_title','Striped Table')
-		@section ('stable_panel_body')
-		@include('widgets.table', array('class'=>'table-striped'))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'stable'))
-	</div>
-	<div class="col-sm-6">
-		@section ('htable_panel_title','Hover Table')
-		@section ('htable_panel_body')
-		@include('widgets.table', array('class'=>'table-hover'))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'htable'))
+	<div>
+		<x-card>
+			<x-slot:title>Bordered Table</x-slot:title>
+			<div class="border border-secondary-200 rounded-lg overflow-hidden">
+				@include('widgets.table', array('class'=>''))
+			</div>
+		</x-card>
 	</div>
 </div>
-	
-<div class="row">
-	<div class="col-sm-6">
-	@section ('ctable_panel_title','Condensed Table')
-		@section ('ctable_panel_body')
-		@include('widgets.table', array('class'=>'table-condensed'))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'ctable'))
+<div class="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
+	<div>
+		<x-card>
+			<x-slot:title>Striped Table</x-slot:title>
+			@include('widgets.table', array('class'=>''))
+		</x-card>
 	</div>
-	<div class="col-sm-6">
-		@section ('atable_panel_title','Condensed, Bordered, Striped Table')
-		@section ('atable_panel_body')
-		@include('widgets.table', array('class'=>'table-condensed table-bordered table-striped'))
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'atable'))
+	<div>
+		<x-card>
+			<x-slot:title>Hover Table</x-slot:title>
+			@include('widgets.table', array('class'=>''))
+		</x-card>
+	</div>
+	<div>
+		<x-card>
+			<x-slot:title>Condensed Table</x-slot:title>
+			@include('widgets.table', array('class'=>''))
+		</x-card>
 	</div>
 </div>
-<div class="row">
-	<div class="col-sm-12">
-		@section ('cotable_panel_title','Coloured Table')
-		@section ('cotable_panel_body')
-		<table class="table table-bordered">
-			<thead>
+<div class="mt-4">
+	<x-card>
+		<x-slot:title>Coloured Table</x-slot:title>
+		<table class="min-w-full divide-y divide-secondary-200">
+			<thead class="bg-secondary-50">
 				<tr>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Address</th>
+					<th class="px-3 py-2 text-left text-xs font-medium text-secondary-500 uppercase tracking-wider">Name</th>
+					<th class="px-3 py-2 text-left text-xs font-medium text-secondary-500 uppercase tracking-wider">Email</th>
+					<th class="px-3 py-2 text-left text-xs font-medium text-secondary-500 uppercase tracking-wider">Address</th>
 				</tr>
 			</thead>
-			<tbody>
-				<tr class="success">
-					<td>John</td>
-					<td>john@gmail.com</td>
-					<td>London, UK</td>
+			<tbody class="bg-white divide-y divide-secondary-200">
+				<tr class="bg-success-50 hover:bg-success-100">
+					<td class="px-3 py-2 text-sm text-success-800">John</td>
+					<td class="px-3 py-2 text-sm text-success-800">john@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-success-800">London, UK</td>
 				</tr>
-				<tr>
-					<td>Wayne</td>
-					<td>wayne@gmail.com</td>
-					<td>Manchester, UK</td>
+				<tr class="hover:bg-secondary-50">
+					<td class="px-3 py-2 text-sm text-secondary-700">Wayne</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">wayne@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">Manchester, UK</td>
 				</tr>
-				<tr class="info">
-					<td>Andy</td>
-					<td>andy@gmail.com</td>
-					<td>Merseyside, UK</td>
+				<tr class="bg-cyan-50 hover:bg-cyan-100">
+					<td class="px-3 py-2 text-sm text-cyan-800">Andy</td>
+					<td class="px-3 py-2 text-sm text-cyan-800">andy@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-cyan-800">Merseyside, UK</td>
 				</tr>
-				<tr>
-					<td>Danny</td>
-					<td>danny@gmail.com</td>
-					<td>Middlesborough, UK</td>
+				<tr class="hover:bg-secondary-50">
+					<td class="px-3 py-2 text-sm text-secondary-700">Danny</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">danny@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">Middlesborough, UK</td>
 				</tr>
-				<tr class="warning">
-					<td>Frank</td>
-					<td>frank@gmail.com</td>
-					<td>Southampton, UK</td>
+				<tr class="bg-warning-50 hover:bg-warning-100">
+					<td class="px-3 py-2 text-sm text-warning-800">Frank</td>
+					<td class="px-3 py-2 text-sm text-warning-800">frank@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-warning-800">Southampton, UK</td>
 				</tr>
-				<tr>
-					<td>Scott</td>
-					<td>scott@gmail.com</td>
-					<td>Newcastle, UK</td>
+				<tr class="hover:bg-secondary-50">
+					<td class="px-3 py-2 text-sm text-secondary-700">Scott</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">scott@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-secondary-700">Newcastle, UK</td>
 				</tr>
-				<tr class="danger">
-					<td>Rickie</td>
-					<td>rickie@gmail.com</td>
-					<td>Burnley, UK</td>
+				<tr class="bg-danger-50 hover:bg-danger-100">
+					<td class="px-3 py-2 text-sm text-danger-800">Rickie</td>
+					<td class="px-3 py-2 text-sm text-danger-800">rickie@gmail.com</td>
+					<td class="px-3 py-2 text-sm text-danger-800">Burnley, UK</td>
 				</tr>
 			</tbody>
-		</table>	
-		@endsection
-		@include('widgets.panel', array('header'=>true, 'as'=>'cotable'))
-	</div>
+		</table>
+	</x-card>
 </div>
 </div>
 @stop
